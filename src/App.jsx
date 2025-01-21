88b75733f4954161d188f56c46fd3a4e5d9bbb50
mohamedfathy32 <mf876026@gmail.com>
2025-01-21 22:32:43 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex  flex-col justify-center items-center h-screen">
+      <h1 className="text-3xl font-bold mb-5">Hello, Fekry</h1>
+      <h1 className="text-3xl font-bold">a7la mesa 3lek</h1>
+    </div>
+  );
+}
+
+export default App;
