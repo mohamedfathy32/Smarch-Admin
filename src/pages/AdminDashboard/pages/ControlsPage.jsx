89e507b6105e8af2ffc4f9d24a0cd04dd72d7f72
fekry <fekry.zakaria1461998@gmail.com>
2025-01-28 04:49:55 +0200

    Login Admin

--- src/pages/AdminDashboard/pages/ControlsPage.jsx
+++ src/pages/AdminDashboard/pages/ControlsPage.jsx
@@ -1,7 +1,18 @@
+import axios from 'axios';
 import React from 'react'
 
 export default function ControlsPage() {
+  const fatchdata = async () => {
+    const response = await axios.get('https://smarch-back-end-nine.vercel.app/reservation',{
+      headers: {
+        authorization: token
+    },
+    });
+    console.log(response.data);
+  }
   return (
-    <div>ControlsPage</div>
+      <div>ControlsPage
+        <button onClick={fatchdata}>Fetch Data</button>
+      </div>  
   )
 }
